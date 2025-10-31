d049f2a88fe8e9ed464f07904585d867f055d52b
chetanascentialabscom-eng <chetan.ascentialabs.com@gmail.com>
2025-10-31 18:01:43 +0530

    add

---
+++ 01_Intro/src/main/java/com/intro/cktechhub/CktechhubApplication.java
@@ -0,0 +1,13 @@
+package com.intro.cktechhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CktechhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CktechhubApplication.class, args);
+	}
+
+}
---
+++ 01_Intro/src/test/java/com/intro/cktechhub/CktechhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intro.cktechhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CktechhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
